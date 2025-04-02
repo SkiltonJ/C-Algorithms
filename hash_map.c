26f7a6614f2a7ccc37310331c101d83901690aa8
Jackson Jaensch <jackson.jaensch14@gmail.com>
2025-04-02 12:51:47 +1030

    Hashmap practice

--- hash_map.c
+++ hash_map.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct {
     char *key;
@@ -8,83 +10,81 @@ typedef struct {
 typedef struct {
     int capacity;
     int size;
-    KeyValuePair** items;
+    KeyValuePair **items;
 } HashMap;
 
-unsigned int    hash(char *key, int capacity)
-{
-    unsigned long   hash = 5381;
+unsigned int hash(char *key, int capacity) {
+    unsigned long hash = 5381;
     int c;
-    
-    while ((c = *key++))
-    {
+
+    while ((c = *key++)) {
         hash = ((hash << 5) + hash) + c;
     }
     return hash % capacity;
 }
 
-HashMap *createHashMap(int capacity)
-{
+HashMap *createHashMap(int capacity) {
     HashMap *map = (HashMap *)malloc(sizeof(HashMap));
-    if (map == NULL)
+    if (map == NULL) {
         return NULL;
+    }
     map->capacity = capacity;
     map->size = 0;
-    map->items = (KeyValuePairs *)calloc(capacity, sizeof(KeyValuePair*));
-    if (map->items == NULL)
-    {
+    map->items = (KeyValuePair **)calloc(capacity, sizeof(KeyValuePair *));
+    if (map->items == NULL) {
         free(map);
         return NULL;
     }
     return map;
 }
 
-void    insertHashMap(HashMap *map, char *key, void *value)
-{
-    if (map == NULL || key == NULL)
+void insertHashMap(HashMap *map, char *key, void *value) {
+    if (map == NULL || key == NULL) {
         return;
-    unsigned int    index = hash(key, map->capacity);
-    while (map->items[index] != NULL)
-    {
-        if (strcmp(map->items[index]->key, key) == 0)
-        {
+    }
+    unsigned int index = hash(key, map->capacity);
+    while (map->items[index] != NULL) {
+        if (strcmp(map->items[index]->key, key) == 0) {
             map->items[index]->value = value;
             return;
         }
-        index = (index + 1) % map-capacity
+        index = (index + 1) % map->capacity;
     }
-    KeyValuePair *item = (KeyValuePair*)malloc(sizeof(KeyValuePair));
-    if (item == NULL)
+    KeyValuePair *item = (KeyValuePair *)malloc(sizeof(KeyValuePair));
+    if (item == NULL) {
         return;
+    }
     item->key = strdup(key);
+    if(item->key == NULL){
+        free(item);
+        return;
+    }
     item->value = value;
     map->items[index] = item;
     map->size++;
 }
 
-void *getHashMap(HashMap *map, char *key)
-{
-    if (map == NULL || key == NULL)
+void *getHashMap(HashMap *map, char *key) {
+    if (map == NULL || key == NULL) {
         return NULL;
-    unsigned int    index = hash(key, map->capacity);
-    
-    while (map->items[index] != NULL)
-    {
-        if (strcmp(map->items[index]->key, key) == 0)
+    }
+    unsigned int index = hash(key, map->capacity);
+
+    while (map->items[index] != NULL) {
+        if (strcmp(map->items[index]->key, key) == 0) {
             return map->items[index]->value;
+        }
         index = (index + 1) % map->capacity;
     }
     return NULL;
 }
 
-void    freeHashMap(HashMap *map)
-{
-    if (map == NULL)
+void freeHashMap(HashMap *map) {
+    if (map == NULL) {
         return;
-    for (int i = 0; i < map->capacity; i++)
-    {
-        if (map->items[i] != NULL)
-        {
+    }
+    for (int i = 0; i < map->capacity; i++) {
+        if (map->items[i] != NULL) {
             free(map->items[i]->key);
             free(map->items[i]);
         }
@@ -93,25 +93,43 @@ void    freeHashMap(HashMap *map)
     free(map);
 }
 
-typedef struct
-{
+/* Hashmap Practice */
+
+typedef struct {
 	char *key;
 	void *value;
 } KeyValuePair;
 
-typedef struct
-{
+typedef struct {
 	int capacity;
 	int size;
-	KeyValuePair** items;
+	KeyValuePair **items;
 } HashMap;
 
-unsigned int	hash(char *key, int capacity)
+unsigned int hash(char *key, int capacity)
 {
-	unsigned long	hash = 5381;
+	unsigned long hash = 5381;
 	int c;
 
 	while ((c = *key++))
 		hash = ((hash << 5) + hash) + c;
-	return (hash % capacity);
+	return hash % capacity;
 }
+
+HashMap *createHashMap(int capacity)
+{
+	HashMap *map = (HashMap*)malloc(sizeof(HashMap));
+	if (map == NULL)
+		return NULL;
+	map->capacity = capacity;
+	map->size = 0;
+	map->items = (KeyValuePair**)calloc(capacity, sizeof(KeyValuePair *));
+	if (map->items == NULL) {
+		free(map);
+		return NULL;
+	}
+	return map;
+}
+
+
+
